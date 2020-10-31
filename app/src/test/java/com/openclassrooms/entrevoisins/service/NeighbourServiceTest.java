bbab87956c254d826ec7119cc5fe3c9ec2407759
ldegans <lionel.degans@shadline.com>
2020-10-31 17:51:44 +0100

    units tests

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -11,8 +11,10 @@ import org.junit.runners.JUnit4;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -39,5 +41,42 @@ public class NeighbourServiceTest {
         Neighbour neighbourToDelete = service.getNeighbours().get(0);
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
+    }
+
+    @Test
+    public void addAndGetFavoriteNeighbourWithSuccess(){
+        Neighbour neighbour= service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(neighbour);
+        assertTrue(service.getFavoritesNeighbours().contains(neighbour));
+    }
+
+    @Test
+    public void deleteFavoriteNeighbourWithSuccess(){
+        Neighbour neighbour= service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(neighbour);
+        service.deleteFavoriteNeighbour(neighbour);
+        assertFalse(service.getFavoritesNeighbours().contains(neighbour));
+    }
+
+    @Test
+    public void findNeighbourByIdWithSuccess(){
+        Neighbour neighbour= service.getNeighbours().get(0);
+        assertEquals(neighbour.getId(), service.findNeighbourById(neighbour.getId()).getId());
+    }
+
+    @Test
+    public void isFavoriteNeighbourWithSuccess(){
+        Neighbour neighbour= service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(neighbour);
+        assertTrue(service.isFavoriteNeighbour(neighbour.getId()));
+    }
+
+    @Test
+    public void createNeighbourWithSuccess(){
+        List<Neighbour> neighbours = service.getNeighbours();
+        Neighbour newNeighbour =  new Neighbour(9855648, "Lionel", "https://i.pravatar.cc/300?u=a042581f3e39026702d", "Saint-Pierre-du-Mont ; 5km",
+                "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner !J'aime les jeux de cartes tels la belote et le tarot..");
+        service.createNeighbour(newNeighbour);
+        assertTrue(neighbours.contains(newNeighbour));
     }
 }
