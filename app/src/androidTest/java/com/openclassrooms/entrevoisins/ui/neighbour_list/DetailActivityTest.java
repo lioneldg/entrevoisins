e8802d982db282542af6be4fb66c6d1fd0c07f73
ldegans <lionel.degans@shadline.com>
2020-11-01 10:31:57 +0100

    Fail Espresso test: DetailActivityTest

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailActivityTest.java
@@ -0,0 +1,36 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.openclassrooms.entrevoisins.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class DetailActivityTest {
+
+    @Rule
+    public ActivityTestRule<ListNeighbourActivity> mActivityTestRule = new ActivityTestRule<>(ListNeighbourActivity.class);
+
+    @Test
+    public void detailActivityTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.list_neighbours),
+                        withParent(withId(R.id.container))));
+        recyclerView.perform(actionOnItemAtPosition(4, click()));
+    }
+}
